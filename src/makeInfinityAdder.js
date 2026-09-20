033d34b44dc3aa5312ec01fd7a5189f1cae2c705
Demian Pishchak <demanpisak191@gmail.com>
2026-09-20 14:56:03 +0300

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function adder(num) {
+    // Якщо функцію викликано без аргументів ()
+    if (num === undefined) {
+      const result = sum;
+
+      sum = 0; // Скидаємо накопичене значення
+
+      return result; // Повертаємо підсумок (число)
+    }
+
+    // Якщо передано число — додаємо його
+    sum += num;
+
+    return adder;
+  };
 }
 
 module.exports = makeInfinityAdder;
